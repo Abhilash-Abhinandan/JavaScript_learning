e9a16f926adac06bf5ebdb62f7f936f6aec11faa
Abhilash-Abhinandan <154450486+Abhilash-Abhinandan@users.noreply.github.com>
2023-12-29 23:26:54 +0000

    DataTypes

---
+++ js_folder/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS codes as newer JS version.
+
+// alert (7 + 7) //  currently we are using nodejs, not a browser. in browser we will get a alert pop-up.
+
+let name = "Abhilash"
+let age = 18
+let isLoggedIn = false
+let state;
+// ********** DataTypes **********
+// number => 2 to the power 53
+// bigint 
+// string => true/false
+//null => standalone value
+// undefined
+//symbol => unique
+//object
+
+/* "typeof" => used to know type of a value */
+console.log(typeof "Abhilash"); //[output] => string
+console.log(typeof 18); // [output] => number
+console.log(typeof null); // [output] => object
+console.log(typeof undefined); // [output] => undefined
